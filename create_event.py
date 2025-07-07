491b0b8d86fee26bd2d4f3fcbe5c955f83c1147d
Adomako N <adomako.nyarko4@gmail.com>
2025-07-07 18:40:34 -0400

    Able to create dates using pdf in google calendar

--- create_event.py
+++ create_event.py
@@ -4,6 +4,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from main import Func
 
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
@@ -30,25 +31,29 @@ def get_creds():
 
 def create_event(service):
     try:
+        f = Func()
+        file = f.openFile()
+        start = f.date_start()
+        end = f.date_end()
         event = {
             'summary': 'Google I/O 2015',
             'location': '800 Howard St., San Francisco, CA 94103',
             'description': 'A chance to hear more about Google\'s developer products.',
             'start': {
-                'dateTime': '2025-07-28T09:00:00-07:00',
-                'timeZone': 'America/Los_Angeles',
+                'dateTime': start,
+                'timeZone': 'America/New_York',
             },
             'end': {
-                'dateTime': '2025-07-29T17:00:00-07:00',
-                'timeZone': 'America/Los_Angeles',
+                'dateTime': end,
+                'timeZone': 'America/New_York',
             },
             'recurrence': [
-                'RRULE:FREQ=DAILY;COUNT=2'
+                'RRULE:FREQ=DAILY;COUNT=1'
             ],
-            'attendees': [
-                {'email': 'lpage@example.com'},
-                {'email': 'sbrin@example.com'},
-            ],
+            # 'attendees': [
+            #     {'email': 'lpage@example.com'},
+            #     {'email': 'sbrin@example.com'},
+            # ],
             'reminders': {
                 'useDefault': False,
                 'overrides': [
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import re
-
+from dateutil import parser
 import pdfplumber
 from date_patterns import time_patterns
 from date_patterns import date_patterns
@@ -7,12 +7,14 @@ from date_patterns import date_patterns
 class Func:
 
     def __init__(self):
+        self.end = None
+        self.start = None
         self.combined_pattern = ''
         self.time = ''
         self.date = ''
         self.dates = []
         self.date_time = []
-
+        self.goog_format = []
 
 
     def openFile(self):
@@ -22,8 +24,8 @@ class Func:
             txt = page.extract_text()
             self.date = r'\b(?:' + '|'.join(date_patterns)
             self.time = r')\b(?:\s+' + '|'.join(time_patterns) + r')?'
-
             self.combined_pattern = '|'.join(date_patterns)
+
             date_only = self.combined_pattern
             date_n_time = self.date + self.time
             self.dates = re.findall(date_only, txt)
@@ -32,12 +34,13 @@ class Func:
 
     def date_start(self):
         self.start = None
-        print(self.date_time)
         for idx, i in enumerate(self.date_time):
             for jdx, j in enumerate(self.date_time):
                 if self.dates[idx] == self.dates[jdx] and i < j:
                     self.start = i
-                    return self.start
+        convert = parser.parse(self.start)
+        self.start = convert.isoformat()
+        return self.start
 
     def date_end(self):
         self.end = None
@@ -46,17 +49,14 @@ class Func:
             for jdx, j in enumerate(self.date_time):
                 if self.dates[idx] == self.dates[jdx] and i < j:
                     self.end = j
-
+        convert = parser.parse(self.end)
+        self.end = convert.isoformat()
         return self.end
-
-
-
 
 if __name__ == '__main__':
     main = Func()
 
     main.openFile()
-    m = main.date_time
-    print(m)
-    main.date_start()
+
+
     main.date_end()
