201dd541f843a724332b6846e999f75145f2e8de
Adomako N <adomako.nyarko4@gmail.com>
2025-07-07 18:03:50 -0400

    updated main handle date comparison

--- main.py
+++ main.py
@@ -1,19 +1,62 @@
 import re
 
 import pdfplumber
-
+from date_patterns import time_patterns
 from date_patterns import date_patterns
-def openFile():
-    get_file = input("File name: ")
-    with pdfplumber.open(get_file) as pdf:
-        page = pdf.pages[0]
-        txt = page.extract_text()
 
-        combined_pattern = '|'.join(date_patterns)
-        dates = re.findall(combined_pattern, txt)
-        print(dates)
-#dd
+class Func:
+
+    def __init__(self):
+        self.combined_pattern = ''
+        self.time = ''
+        self.date = ''
+        self.dates = []
+        self.date_time = []
+
+
+
+    def openFile(self):
+        get_file = input("File name: ")
+        with pdfplumber.open(get_file) as pdf:
+            page = pdf.pages[0]
+            txt = page.extract_text()
+            self.date = r'\b(?:' + '|'.join(date_patterns)
+            self.time = r')\b(?:\s+' + '|'.join(time_patterns) + r')?'
+
+            self.combined_pattern = '|'.join(date_patterns)
+            date_only = self.combined_pattern
+            date_n_time = self.date + self.time
+            self.dates = re.findall(date_only, txt)
+            self.date_time = re.findall(date_n_time, txt)
+            return self.date_time
+
+    def date_start(self):
+        self.start = None
+        print(self.date_time)
+        for idx, i in enumerate(self.date_time):
+            for jdx, j in enumerate(self.date_time):
+                if self.dates[idx] == self.dates[jdx] and i < j:
+                    self.start = i
+                    return self.start
+
+    def date_end(self):
+        self.end = None
+
+        for idx, i in enumerate(self.date_time):
+            for jdx, j in enumerate(self.date_time):
+                if self.dates[idx] == self.dates[jdx] and i < j:
+                    self.end = j
+
+        return self.end
+
+
 
 
 if __name__ == '__main__':
-    openFile()
+    main = Func()
+
+    main.openFile()
+    m = main.date_time
+    print(m)
+    main.date_start()
+    main.date_end()
